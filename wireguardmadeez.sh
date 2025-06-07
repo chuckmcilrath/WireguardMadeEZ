1368eb38956400a58d5ded508518e551aafd2d7e
chuckmcilrath <chuckmcilrath@gmail.com>
2025-06-07 16:07:02 -0700

    Update wireguardmadeez.sh

--- wireguardmadeez.sh
+++ wireguardmadeez.sh
@@ -13,3 +13,33 @@
 resolved_path=/etc/systemd/resolved.conf
 net_interf=/etc/network/interfaces
 wg_port_num=/etc/wireguard/"$wg_interf"
+
+####################
+# GLOBAL FUNCTIONS #
+####################
+
+# Check if user is Root.
+check_root() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "This script must be run as root. Please use sudo or log in as root." >&2
+		exit 1
+	fi
+}
+
+# Check if user entered IP is valid
+is_valid_ip() {
+	local ip=$1
+	local IFS='.'
+	local -a octets=($ip)
+
+	[[ "${octets[0]}" -eq 127 ]] && return 1
+	[[ ${#octets[@]} -ne 4 ]] && return 1
+
+	# Check each octet is between 0 and 255
+	for octet in "${octets[@]}"; do
+		[[ ! "$octet" =~ ^[0-9]+$ ]] && return 1
+		((octet < 0 || octet > 255)) && return 1
+	done
+
+return 0
+}
