0c89c432f737babd5182d222e9df956fffec4063
chuckmcilrath <chuckmcilrath@gmail.com>
2025-06-07 16:22:35 -0700

    Update wireguardmadeez.sh

--- wireguardmadeez.sh
+++ wireguardmadeez.sh
@@ -18,7 +18,7 @@ wg_port_num=/etc/wireguard/"$wg_interf"
 # GLOBAL FUNCTIONS #
 ####################
 
-# Check if user is Root.
+# Check for root permissions.
 check_root() {
 	if [[ $EUID -ne 0 ]]; then
 		echo "This script must be run as root. Please use sudo or log in as root." >&2
@@ -42,4 +42,39 @@ is_valid_ip() {
 	done
 
 return 0
+}
+
+# Check the user's CIDR input to make sure it's within 0-32
+cidr_check() {
+	local cidr=$1
+ 
+	[[ $cidr =~ ^[0-9]+$ ]] || return 1
+	((cidr >= 0 && cidr <= 32))
+}
+
+# Check to see if an app is installed.
+check_install() {
+	local install_name="$1"
+
+	echo "looking for $install_name..."
+	if ! dpkg -l | awk '{print $2}' | grep -xq "$install_name"; then
+		echo "Installing $install_name..."
+		apt install $install_name -y &> /dev/null
+		if dpkg -l | awk '{print $2}' | grep -xq "$install_name"; then
+			echo "$install_name has been successfully installed."
+		else
+			echo "Installation failed. Please clear the error and try again."
+		exit 1
+		fi
+	else
+		echo "$install_name is already installed. Continuing..."
+	fi
+}
+
+# Check user input is 256-bit key
+key_check() {
+    local key="$1"
+	
+    [[ "$key" =~ ^[A-Za-z0-9+/]{43}=$ ]] && return 0
+    return 1
 }
