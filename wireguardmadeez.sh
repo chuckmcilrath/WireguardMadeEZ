6c3626db7d60c112ce5ab58c8318270444672bb7
chuckmcilrath <chuckmcilrath@gmail.com>
2025-07-15 22:36:01 -0700

    Update wireguardmadeez.sh

--- wireguardmadeez.sh
+++ wireguardmadeez.sh
@@ -599,17 +599,6 @@ sub_3.3.2_change_ip() {
 	&& systemctl restart wg-quick@${config_basename}.service
 }
 
-main_4_more_networks_loop() {
-	echo "Configuration file for "$wg_port_name" has been made."
-	while true; do
-		check_user_input_y_N $' At this time, Would you like to add more Allowed Networks? (y/N)\n: ' || return 1
-		check_user_input $'Please enter the Allowed Network (Note: 0.0.0.0 is full tunnel. Please use a 0 in the 4th octet)\n: ' allowed_ip_add is_valid_ip \
-		&& sed -i "/^AllowedIPs/s|$|, $allowed_ip_add|" "$config_path"
-		check_user_input $'Please enter the CIDR of your Allowed Network\n: ' allowed_cidr_add cidr_check \
-		&& sed -i "/^AllowedIPs/s|$|/$allowed_cidr_add|" "$config_path"
- 	done
-}
-
 main_4_peer_config() {
 	if [ -f "$config_path" ]; then   
 		cat <<EOF > "$config_path"
@@ -626,9 +615,17 @@ EOF
 	fi
 }
 
-main_4_more_networks_sed() {
-	
+main_4_more_networks_loop() {
+	echo "Configuration file for "$wg_port_name" has been made."
+	while true; do
+		check_user_input_y_N $' At this time, Would you like to add more Allowed Networks? (y/N)\n: ' || return 1
+		check_user_input $'Please enter the Allowed Network (Note: 0.0.0.0 is full tunnel. Please use a 0 in the 4th octet)\n: ' allowed_ip_add is_valid_ip \
+		&& sed -i "/^AllowedIPs/s|$|, $allowed_ip_add|" "$config_path"
+		check_user_input $'Please enter the CIDR of your Allowed Network\n: ' allowed_cidr_add cidr_check \
+		&& sed -i "/^AllowedIPs/s|$|/$allowed_cidr_add|" "$config_path"
+ 	done
 }
+
 
 main_5_menu() {
 	echo
@@ -750,7 +747,6 @@ while true; do
 			check_user_input $'Please enter the Port number the Wiregard Server is using\n(Default port is 51820): ' port_num port_num_check
 			main_4_peer_config
 			main_4_more_networks_loop
-			main_4_more_networks_sed
 			print_public_key_set_aliases
 			enable_wg
 		;;
